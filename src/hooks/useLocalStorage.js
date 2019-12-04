3f60a64c0b5f8ade1098d7ba65da4c367370cbdd
AbdelIdir <homeworkoracle@protonmail.com>
2019-12-04 14:35:21 +0100

    Main hooks done,finished the setValue function, now working on toggling classes

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
+const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    // Get from local storage by key
+    const item = window.localStorage.getItem(key);
+    // Parse and return stored json or, if undefined, return initialValue
+    return item ? JSON.parse(item) : initialValue;
+  });
 
+  const setValue = value => {
+    setStoredValue(value);
 
-const useLocalStorage = (key,value) => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
 
-
-    
-}
+  return [storedValue,setValue];
+};
